b893922010ae65e1167bcd0a1408a3bab1bc0e90
EasyBoyGame <easygameboyiphone@gmail.com>
2025-04-06 19:34:00 +0200

    update

--- src/routes/checkin/failure/+page.svelte
+++ src/routes/checkin/failure/+page.svelte
@@ -1,6 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-    <body>
-        <h1>FAILURE</h1>
-    </body>
-</html>
+<script lang="ts">
+    export let status: string;
+    export let kartenNr: string | null;
+  </script>
+  
+  <h1>❌ Check-in failed</h1>
+  <p>Your card number is: {kartenNr}</p>
+  <p>Please try again or ask for help.</p>
+  
--- src/routes/checkin/success/+page.svelte
+++ src/routes/checkin/success/+page.svelte
@@ -1,6 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-    <body>
-        <h1>SUCCESS</h1>
-    </body>
-</html>
+<script lang="ts">
+    export let status: string;
+    export let kartenNr: string | null;
+  </script>
+  
+  <h1>✅ Check-in successful!</h1>
+  <p>Welcome, your card number is: {kartenNr}</p>
+  <p>Enjoy the event 🎉</p>
+  
