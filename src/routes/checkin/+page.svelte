94c4409f146fb328dcb12ee37b8da04f5228bdb6
EasyBoyGame <easygameboyiphone@gmail.com>
2025-06-18 11:44:51 +0200

    update

--- src/routes/checkin/+page.svelte
+++ src/routes/checkin/+page.svelte
@@ -2,8 +2,11 @@
   import { onMount } from 'svelte';
   import { goto } from '$app/navigation';
 
+
+
 </script>
 
 <main class="p-4">
   <h1 class="text-2xl font-bold mb-4">This is checkin</h1>
 </main>
+
