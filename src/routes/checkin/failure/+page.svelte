195eaffcae97b167d7bc99c44a6107176c1b85dc
EasyBoyGame <easygameboyiphone@gmail.com>
2025-06-18 14:46:25 +0200

    update

--- src/routes/checkin/failure/+page.svelte
+++ src/routes/checkin/failure/+page.svelte
@@ -1,7 +1,3 @@
-<script lang="ts">
-    export let status: string;
-  </script>
-  
-  <h1>❌ Check-in failed</h1>
-  <p>Please try again or ask for help.</p>
+<h1>❌ Check-in failed</h1>
+<p>Please try again or ask for help.</p>
   
--- src/routes/checkin/success/+page.svelte
+++ src/routes/checkin/success/+page.svelte
@@ -1,9 +1,2 @@
-<script lang="ts">
-  import { goto } from '$app/navigation';
-  import { onMount } from 'svelte';
-
-
-</script>
-
 <h1>✅ Check-in successful!</h1>
 <p>Welcome, enjoy the event 🎉</p>
